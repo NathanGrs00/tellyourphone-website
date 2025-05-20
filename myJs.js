945dcf328dcf809b8d77ad4247e1b321f2a55728
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-20 13:26:09 +0200

    add parallax effect to index.html and initialize parallax functionality in myJs.js

--- myJs.js
+++ myJs.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+});
