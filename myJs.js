5239f42d42f5c91a008b834a2fe72d5d8c15b726
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-22 12:11:04 +0200

    enhance phone-details.html with order summary and specifications sections; update myJs.js for collapsible and select initialization; adjust homepage.css for layout and styling improvements

--- myJs.js
+++ myJs.js
@@ -12,3 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+});
