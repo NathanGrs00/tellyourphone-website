036edab2776a68825838ea3d552d4f9cf0182b4a
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-23 19:28:24 +0200

    refactor myJs.js to streamline initialization of UI components; consolidate event listeners for improved performance

--- myJs.js
+++ myJs.js
@@ -1,24 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {});
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, {});
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {});
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, {});
+    M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'));
+    M.Parallax.init(document.querySelectorAll('.parallax'));
+    M.Carousel.init(document.querySelectorAll('.carousel'));
+    M.Collapsible.init(document.querySelectorAll('.collapsible'));
+    M.FormSelect.init(document.querySelectorAll('select'));
 });
